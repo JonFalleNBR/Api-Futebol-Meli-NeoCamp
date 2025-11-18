24d9c1f672f277739404ee1fef4da408dee4f22a
JonFalleNBR <jonathan.leoesdejuda@gmail.com>
2025-11-18 11:43:20 -0300

    feat-inclusao_mapper

---
+++ src/main/java/com/jonathan/futebol_api/core/mapper/ClubeMapper.java
@@ -0,0 +1,42 @@
+package com.jonathan.futebol_api.core.mapper;
+
+import com.jonathan.futebol_api.adapter.dto.ClubeRequestDto;
+import com.jonathan.futebol_api.adapter.dto.ClubeResponseDTO;
+import com.jonathan.futebol_api.core.entity.Clube;
+
+public class ClubeMapper {
+
+// Main Method responsible to Convert the ClubRequestDTO to Clube Entity
+    public static Clube toEntity(ClubeRequestDto requestDto){
+        Clube clube = new Clube();
+
+        clube.setNome(requestDto.nome());
+        clube.setFk_estadio(requestDto.idEstadio());
+        clube.setEstado(requestDto.estado());
+        clube.setAtivo(true);
+        clube.setVitorias(requestDto.vitorias());
+        clube.setEmpates(requestDto.empates());
+        clube.setDerrotas(requestDto.derrotas());
+
+
+        return clube;
+    }
+
+    public static ClubeResponseDTO  toResponseDto (Clube clube){
+            return new ClubeResponseDTO(
+                    clube.getNome(),
+                    clube.getEstado(),
+                    clube.getFk_estadio().toString(),
+                    clube.getVitorias(),
+                    clube.getEmpates(),
+                    clube.getDerrotas(),
+                    clube.getAtivo()
+
+            );
+
+    }
+
+
+
+
+}
