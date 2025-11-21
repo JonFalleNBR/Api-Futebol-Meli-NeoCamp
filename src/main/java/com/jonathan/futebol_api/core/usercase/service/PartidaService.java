d0b9d5b00c7e91785547607a77012b84d8efdef5
JonFalleNBR <jonathan.leoesdejuda@gmail.com>
2025-11-21 17:03:50 -0300

    feat-logica_CRUD_Partida

--- src/main/java/com/jonathan/futebol_api/core/usercase/service/PartidaService.java
+++ src/main/java/com/jonathan/futebol_api/core/usercase/service/PartidaService.java
@@ -1,13 +1,18 @@
 package com.jonathan.futebol_api.core.usercase.service;
 
-
 import com.jonathan.futebol_api.adapter.repository.ClubeRepository;
 import com.jonathan.futebol_api.adapter.repository.EstadioRepository;
 import com.jonathan.futebol_api.adapter.repository.PartidaRepository;
+import com.jonathan.futebol_api.core.entity.Clube;
+import com.jonathan.futebol_api.core.entity.Estadio;
 import com.jonathan.futebol_api.core.entity.Partida;
+import com.jonathan.futebol_api.utils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PartidaService {
@@ -22,14 +27,44 @@ public class PartidaService {
     private EstadioRepository estadioRepository;
 
 
-    public Partida cadastrarNovaPartida(Partida partida){
+    public Partida cadastrarNovaPartida(Partida partida, Estadio estadio){
+        Clube clubeMandante = partida.getClubeMandante();
+        Clube clubeVisitante = partida.getClubeVisitante();
+        Estadio estadioPartida = partida.getEstadio();
 
-        // TODO validar se clube e estadio existem
+
+        if(!clubeRepository_.existsById(clubeMandante.getIdClube())){
+            throw new RuntimeException(utils.MensagensException.CLUBE_INEXISTENTE.getMensagem());
+
+        }
+        if(!clubeRepository_.existsById(clubeVisitante.getIdClube())){
+            throw new RuntimeException(utils.MensagensException.CLUBE_INEXISTENTE.getMensagem());
+        }
+
+
+        if (!estadioRepository.existsById(estadioPartida.getIdEstadio())){
+            throw new RuntimeException(utils.MensagensException.ESTADIO_INEXISTENTE.getMensagem());
+
+        }
+
 
         return partidaRepository_.save(partida);
     }
 
-    // TODO fazer metodos de Busca por id e listar todas as partidas - nao havera delecao de partidas e nem edicao.
+
+    public Optional<Partida> listaPartidaPorId(Long id){
+        Partida partida = partidaRepository_.findById(id)
+                .orElseThrow(() -> new RuntimeException(utils.MensagensException.PARTIDA_INVALIDA.getMensagem()));
+
+        return partidaRepository_.findById(id);
+    }
+
+    public Page<Partida> listaTodasPartidas(Pageable pageable){
+        return partidaRepository_.findAll(pageable);
+
+    }
+
+    // TODO fazer melhoria dos logs de Exception - substituir o RunTimeException
 
 
 
--- src/main/java/com/jonathan/futebol_api/utils.java
+++ src/main/java/com/jonathan/futebol_api/utils.java
@@ -5,7 +5,9 @@ public class utils {
     public enum MensagensException{
         CLUBE_INEXISTENTE("Clube nao encontrado na nossa base de Dados"),
 
-        ESTADIO_INEXISTENTE("Estadio nao encontrado na base de Dados");
+        ESTADIO_INEXISTENTE("Estadio nao encontrado na base de Dados"),
+
+        PARTIDA_INVALIDA("Partida nao encontrada em nossa base de Dados");
 
         private final String mensagem;
 
