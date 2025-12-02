ecc0adae4b074f07fb15d68f20f424b3059896d7
JonFalleNBR <jonathan.leoesdejuda@gmail.com>
2025-12-02 20:34:36 -0300

    fix-correcao_bug_busca_personalizada_Specification

--- src/main/java/com/jonathan/futebol_api/helper/ClubeSpecifications.java
+++ src/main/java/com/jonathan/futebol_api/helper/ClubeSpecifications.java
@@ -7,19 +7,20 @@ public class ClubeSpecifications {
 
     public static Specification<Clube> nomeContains(String nome){
         return(root, query, cb) ->
-                    cb.like(cb.lower(root.get("nome")), "%" + nome.toLowerCase());
+                    cb.like(cb.lower(root.get("nome")),
+                            "%" + nome.toLowerCase() + "%") ;
     }
 
     public static Specification<Clube> estadoEquals(String estado){
         return((root, query, cb) ->
-                      cb.like(cb.lower(root.get("estado")), estado.toLowerCase()));
+                      cb.equal(cb.upper(root.get("estado")), estado.toUpperCase()));
 
     }
 
 
     public static Specification<Clube> ativoEquals(Boolean ativo){
         return (root, query, cb) ->
-                    cb.equal(root.get("ativp"), ativo);
+                    cb.equal(root.get("ativo"), ativo);
     }
 
 }
